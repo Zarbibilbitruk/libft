22c4c7d79b9a603d3d7905d539e3d095aa2da488
Titouan Autin--Desbois <tautin--@paul-f3Ar7s5.clusters.42paris.fr>
2024-05-23 17:02:53 +0200

    strncmp no more issues

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:01:16 by tautin--          #+#    #+#             */
-/*   Updated: 2024/05/23 12:03:01 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:00:45 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
+	unsigned char	*ft_s1;
+	unsigned char	*ft_s2;
 
 	i = 0;
+	ft_s1 = (unsigned char *)s1;
+	ft_s2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	while ((s1[i] || s2[i]) && i < n)
+	while ((ft_s1[i] || ft_s2[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (ft_s1[i] != ft_s2[i])
+			return (ft_s1[i] - ft_s2[i]);
 		i++;
 	}
 	if (i != n)
-		return (*s1 - *s2);
-	return (*s1 - *s2);
+		return (*ft_s1 - *ft_s2);
+	return (*ft_s1 - *ft_s2);
 }
