a900065c32d6eb2be9148c04a84502a58cac66fe
Titouan Autin--Desbois <tautin--@paul-f3Ar4s9.clusters.42paris.fr>
2024-05-30 18:11:16 +0200

    chier dans benn

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:06:42 by tautin--          #+#    #+#             */
-/*   Updated: 2024/05/30 17:57:05 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/05/30 18:10:25 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ size_t	len_nbr(int n)
 	if (n == -2147483648)
 		return (11);
 	i = 0;
+	if (n < 0)
+	{
+		i++;
+		n = n * -1;
+	}
 	while (n > 0)
 	{
 		n = n / 10;
@@ -40,9 +45,15 @@ char	*ft_itoa(int n)
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
+	if (n < 0)
+	{
+		str[i] = '-';
+		i++;
+		n = n * -1;
+	}
 	while (i < len)
 	{
-		str[i] = (n / (10 ^ ((len - 1) - i))) + 48;
+		str[i] = (n / (10 ^ ((len - 1) - i))) + 48; //1 seul chiffre  la fois et moi je fais tout cpmme un con
 		i++;
 	}
 	str[len] = '\0';
