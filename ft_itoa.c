379d8861851f9938e72d3b39af454198dd4be1fe
Titouan Autin--Desbois <tautin--@paul-f3Ar4s9.clusters.42paris.fr>
2024-05-30 18:54:03 +0200

    itoa fonctionnel

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:06:42 by tautin--          #+#    #+#             */
-/*   Updated: 2024/05/30 18:10:25 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/05/30 18:53:05 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ size_t	len_nbr(int n)
 	if (n == -2147483648)
 		return (11);
 	i = 0;
+	if (n == 0)
+		return (1);
 	if (n < 0)
 	{
 		i++;
@@ -32,30 +34,42 @@ size_t	len_nbr(int n)
 	return (i);
 }
 
-// fonction check negatif ou pas
+static unsigned int	is_neg(int n, char *str)
+{
+	unsigned int	nbr;
+
+	if (n < 0)
+	{
+		str[0] = '-';
+		nbr = n * -1;
+	}
+	else
+		nbr = n;
+	return (nbr);
+}
 
 char	*ft_itoa(int n)
 {
-	size_t	i;
-	size_t	len;
-	char	*str;
+	size_t			len;
+	char			*str;
+	unsigned int	nbr;
 
-	i = 0;
 	len = len_nbr(n);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	if (n < 0)
+	if (n == 0)
 	{
-		str[i] = '-';
-		i++;
-		n = n * -1;
+		str[0] = 48;
+		return (str);
 	}
-	while (i < len)
-	{
-		str[i] = (n / (10 ^ ((len - 1) - i))) + 48; //1 seul chiffre  la fois et moi je fais tout cpmme un con
-		i++;
-	}
+	nbr = is_neg(n, str);
 	str[len] = '\0';
+	while (nbr)
+	{
+		str[len - 1] = (nbr % 10) + 48;
+		nbr = nbr / 10;
+		len--;
+	}
 	return (str);
 }
