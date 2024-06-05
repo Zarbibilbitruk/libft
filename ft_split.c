38b4bea4a2e56005f9388edae26d370855c673dd
Titouan Autin--Desbois <tautin--@paul-f3Ar4s6.clusters.42paris.fr>
2024-06-05 15:16:38 +0200

    split prout

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 12:20:15 by tautin--          #+#    #+#             */
-/*   Updated: 2024/06/03 18:01:13 by tautin--         ###   ########.fr       */
+/*   Updated: 2024/06/05 15:16:20 by tautin--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ static size_t	count_str(char const *s, char c)
 	int		i;
 	size_t	nbr;
 
+	if (!s)
+		return (0);
 	i = 0;
-	nbr = 1;
+	nbr = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		while (s[i] == c)
+			i++;
+		if (s[i])
 			nbr++;
-		i++;
+		while (s[i] != c && s[i])
+			i++;
 	}
 	return (nbr);
 }
@@ -32,11 +37,26 @@ char	**ft_split(char const *s, char c)
 {
 	char	**tab;
 	size_t	string_len;
-	int		i;
+	int		j;
 
 	tab = (char **)malloc((count_str(s, c) + 1) * sizeof(char *));
-	if (!s || !tab)
+	if (!tab || !s)
 		return (NULL);
-	i = 0;
-	
+	j = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			if (ft_strchr(s, c) == NULL)
+				string_len = ft_strlen(s);
+			else
+				string_len = ft_strchr(s, c) - s;
+			tab[j++] = ft_substr(s, 0, string_len);
+			s += string_len;
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
 }
